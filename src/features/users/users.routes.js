50ad2a36848b977f3ab4520237bd284fb9a3bb88
nibras1998 <mohdnibras1998@gmail.com>
2026-05-29 15:50:24 +0530

    feat: revert staff permissions back to BRAND_MANAGER only

--- src/features/users/users.routes.js
+++ src/features/users/users.routes.js
@@ -7,7 +7,7 @@ const router = Router();
 router.use(authenticate);
 
 router.get("/", authorize("ADMIN", "BRAND_MANAGER", "BRANCH_MANAGER"), ctrl.getAll);
-router.post("/", authorize("BRANCH_MANAGER"), ctrl.create);
-router.delete("/:id", authorize("BRANCH_MANAGER"), ctrl.remove);
+router.post("/", authorize("ADMIN", "BRAND_MANAGER"), ctrl.create);
+router.delete("/:id", authorize("ADMIN", "BRAND_MANAGER"), ctrl.remove);
 
 module.exports = router;
